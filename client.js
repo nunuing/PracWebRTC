3a19c3a9fee539ef4cc51e264241ece6f1428336
eycho <chw3872@gmail.com>
2023-12-24 21:14:41 +0900

    update client part

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
 const { useState } = require("react");
 
-const [pc, setPc] = useState<RTCPeerConnection>();
+
+const [pc, setPc] = useState<RTCPeerConnection>(null);
+const [socket, setSocket] = useState<SocketIOClient.I
