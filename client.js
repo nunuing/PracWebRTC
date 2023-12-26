d5e430a81119a52c518b4b722927fb0bbcfb23c4
eycho <chw3872@gmail.com>
2023-12-26 18:17:10 +0900

    update client code

--- client.js
+++ client.js
@@ -1,5 +1,33 @@
-const { useState } = require("react");
+import {useEffect, useRef} from "react";
+import {useParams} from "react-router-dom";
+import {Socket} from "socket.io-client";
 
+const VideoCall = () => {
+    const socketRef = useRef<Socket>(null);                     //소켓 정보
+    const myVideoRef = useRef<HTMLVideoElement>(null);          //자신의 비디오
+    const remoteVideoRef = useRef<HTMLVideoElement>(null);      //상대방 비디오
+    const peerRef = useRef<RTCPeerConnection>(null);              //peer connection
 
-const [pc, setPc] = useState<RTCPeerConnection>(null);
-const [socket, setSocket] = useState<SocketIOClient.I
+    // 방으로 진입하면 해당 방 번호를 url parameter로 전달
+    const {roomName} = useParams();
+
+    useEffect(() => {   
+        socketRef.current = io("localhost:3000");               //소켓 연결
+        peerRef.current = new RTCPeerConnection({
+            iceServers: [
+                {
+                urls: "stun:stun.l.google.com:19302",
+                },
+            ],
+        });
+    }, []);
+
+    return (
+        <div>
+            <video ref={myVideoRef} autoPlay />
+            <video ref={remoteVideoRef} autoPlay />
+        </div>
+    );
+};
+
+export default VideoCall;
